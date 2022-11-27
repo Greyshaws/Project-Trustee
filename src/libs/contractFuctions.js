ac643244555700777ed3b6fe4d9598bd45bc4201
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-11-27 16:57:38 +0100

    feat: improve contract and create interface to intract with our smart contract

---
+++ src/libs/contractFuctions.js
@@ -0,0 +1,72 @@
+import { ethers, providers } from "ethers";
+import contractABI from "./contract_abi.json";
+import { providerOptions } from "./config";
+
+const contract = String(process.env.NEXT_PUBLIC_CONTRACT)
+
+
+export async function createTrust(interval, title, beneficiaries) {
+
+    const provider = new ethers.providers.Web3Provider(window?.ethereum, 'any');
+
+    const signer = provider.getSigner();
+
+    const currentContract = new ethers.Contract(contract, contractABI, signer);
+
+    const result = await currentContract.createTrust(interval, title, beneficiaries);
+
+    await result.wait()
+
+    return result
+}
+
+
+export async function getMyTrust() {
+
+    const provider = new ethers.providers.Web3Provider(window?.ethereum, 'any');
+
+    const signer = provider.getSigner();
+
+    const currentContract = new ethers.Contract(contract, contractABI, signer);
+
+    const result = await currentContract.getMyTrust();
+
+    await result.wait()
+
+    return result
+
+}
+
+
+export async function updateMyTrustBeneficiaries(indexes, beneficiaries) {
+
+    const provider = new ethers.providers.Web3Provider(window?.ethereum, 'any');
+
+    const signer = provider.getSigner();
+
+    const currentContract = new ethers.Contract(tokenContract, contractABI, signer);
+
+    const result = await currentContract.updateMyTrustBeneficiaries(indexes, beneficiaries);
+
+    await result.wait()
+
+    return result
+    
+}
+
+
+export async function addToMyTrustBeneficiaries(indexes, beneficiaries) {
+
+    const provider = new ethers.providers.Web3Provider(window?.ethereum, 'any');
+
+    const signer = provider.getSigner();
+
+    const currentContract = new ethers.Contract(contract, contractABI, signer);
+
+    const result = await currentContract.addToMyTrustBeneficiaries(indexes, beneficiaries);
+
+    await result.wait()
+
+    return result
+
+}
