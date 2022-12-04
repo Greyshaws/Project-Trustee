139eac50c6acb38f5dcbb9cb3658c821e842b00a
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-12-04 20:31:52 +0100

    Secure contract

--- pages/new-trust.js
+++ pages/new-trust.js
@@ -1,4 +1,5 @@
-import { Button, Card, Grid, Typography } from "@mui/material";
+import { useContext } from "react";
+import { Card  } from "@mui/material";
 import Head from "next/head";
 import NavBar from "../src/components/app/Layout/Navbar";
 import styles from "../styles/Home.module.css";
@@ -6,9 +7,14 @@ import PaddedContainer from "../src/components/app/PaddedContainer";
 import CreateTrust from "../src/components/app/Trust/CreateTrust";
 import Footer from "../src/components/app/Layout/Footer";
 import { useRouter } from "next/router";
+import PleaseLogin from "../src/components/app/PleaseLogin";
+import { Web3Context } from "../src/context/Web3Context";
 
 const NewTrust = () => {
-  const router = useRouter();
+
+  const { accounts, connect } = useContext(Web3Context);
+
+  if (!accounts) return (<PleaseLogin connect={connect}  />)
 
   return (
     <>
